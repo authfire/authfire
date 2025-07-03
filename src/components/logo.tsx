0c2bbe12030908e745dd7e5a11a2136358a75abc
Vinoth Kannan <svkn.87@gmail.com>
2025-07-03 17:17:17 +0530

    Refactor: Use dynamic logomark image URL in Logo component

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,4 +1,4 @@
-import { appName } from "@/lib/const"
+import { appName, logomarkImageUrl } from "@/lib/const"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -6,8 +6,8 @@ export default function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 font-bold text-lg">
       <Image
-        src="/images/authfire-logomark-64px.png"
-        alt="AuthFire Logomark"
+        src={ logomarkImageUrl}
+        alt={ `${appName} Logomark` }
         width={32}
         height={32}
         className="h-8 w-8"
--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,5 +1,6 @@
 export const appName = process.env.NEXT_PUBLIC_APP_NAME || "AuthFire";
 export const baseUrl = `https://${process.env.NEXT_PUBLIC_HOSTNAME}`;
+export const logomarkImageUrl = process.env.NEXT_PUBLIC_LOGOMARK_IMAGE_URL || `${baseUrl}/images/authfire-logomark-64px.png`;
 export const privacyPolicyUrl = process.env.NEXT_PUBLIC_PRIVACY_POLICY_URL || `${baseUrl}/privacy-policy`;
 export const termsOfServiceUrl = process.env.NEXT_PUBLIC_TERMS_OF_SERVICE_URL || `${baseUrl}/terms-of-service`;
 export const idTokenVerificationUrl = process.env.NEXT_PUBLIC_ID_TOKEN_VERIFICATION_URL;
