37e315bd222bca51aee9214a0f93817763a306ee
Vinoth Kannan <svkn.87@gmail.com>
2025-07-03 18:41:05 +0530

    Refactor: Update logo component to use new logo image URL and remove old logomark image

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,4 +1,4 @@
-import { appName, logomarkImageUrl } from "@/lib/const"
+import { appName, logoImageUrl } from "@/lib/const"
 import Image from "next/image"
 import Link from "next/link"
 
@@ -6,13 +6,12 @@ export default function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2 font-bold text-lg">
       <Image
-        src={ logomarkImageUrl}
-        alt={ `${appName} Logomark` }
-        width={32}
-        height={32}
-        className="h-8 w-8"
+        src={ logoImageUrl}
+        alt={ `${appName} Logo` }
+        width={320}
+        height={64}
+        className="h-8 w-auto"
       />
-      {appName}
     </Link>
   )
 }
