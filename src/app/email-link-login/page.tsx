630ee755cb82da1714f9ae0ecf0e15c7a7abfdad
Vinoth Kannan <svkn.87@gmail.com>
2025-08-08 17:43:38 +0530

    refactor: email link login component to improve user experience and error handling

--- src/app/email-link-login/page.tsx
+++ src/app/email-link-login/page.tsx
@@ -1,70 +1,29 @@
 "use client";
 
-import LinkToAuthfire from "@/components/link-to-authfire";
-import { Button } from "@/components/ui/button";
-import { appName } from "@/lib/const";
 import { auth } from "@/lib/firebase";
-import { signOut, useCurrentUser } from "@authfire/reactfire";
 import { logEvent } from "firebase/analytics";
 import { isSignInWithEmailLink, signInWithEmailLink } from "firebase/auth";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { redirect, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import { toast } from "sonner";
 
-const handleSignOut = async () => {
-  await signOut(auth);
-}
-
-export default function Home() {
-  const { user } = useCurrentUser(auth);
-  const searchParams = useSearchParams();
-  const [mode, setMode] = useState<string|null>();
-  const [signingIn, setSigningIn] = useState(true);
-
-  useEffect(() => {
-    const currentUrl = window.location.href;
-    const isSignInLink = isSignInWithEmailLink(auth, currentUrl)
-    if (!isSignInLink) {
-      setSigningIn(false);
-      return;
-    }
-
-    let email = window.localStorage.getItem('emailForSignIn');
-    if (!email) {
-      email = window.prompt('Please provide your email for confirmation');
-    }
-
-    if (!email) {
-      setSigningIn(false);
-      return;
-    }
-
-    signInWithEmailLink(auth, email, currentUrl)
-      .then((userCredential) => {
-        window.localStorage.removeItem('emailForSignIn');
-        logEvent(auth, 'signed_in', {
-          uid: userCredential.user.uid,
-        });
-      }).catch((error) => {
-        console.error("Error signing in with email link:", error);
-        setSigningIn(false);
-      });
-  }, [mode]);
-
-  setMode(searchParams.get("mode"));
-  if (mode === "signIn") {
-    if (signingIn) {
-      return (
-        <div className="flex flex-col items-center justify-items-center h-full gap-64">
-          <main className="flex flex-col flex-auto gap-[32px] row-start-2 items-center sm:items-start">
-            <div className="list-inside list-decimal text-sm/6 text-center sm:text-left">
-              <div className="mb-2 tracking-[-.01em]">
-                Signing in...
-              </div>
+export default function Page() {
+  const Loading =
+      <div className="flex flex-col items-center justify-items-center h-full gap-64">
+        <main className="flex flex-col flex-auto gap-[32px] row-start-2 items-center sm:items-start">
+          <div className="list-inside list-decimal text-sm/6 text-center sm:text-left">
+            <div className="mb-2 tracking-[-.01em]">
+              Loading...
             </div>
-          </main>
-        </div>
-      )
-    } else {
+          </div>
+        </main>
+      </div>
+
+  function SignIn() {
+    const searchParams = useSearchParams();
+    const mode = searchParams.get("mode");
+
+    if (mode !== "signIn") {
       return (
         <div className="flex flex-col items-center justify-items-center h-full gap-64">
           <main className="flex flex-col flex-auto gap-[32px] row-start-2 items-center sm:items-start">
@@ -77,44 +36,73 @@ export default function Home() {
         </div>
       );
     }
+
+    const currentUrl = window.location.href;
+    const isSignInLink = isSignInWithEmailLink(auth, currentUrl)
+    if (!isSignInLink) {
+      return (
+        <div className="flex flex-col items-center justify-items-center h-full gap-64">
+          <main className="flex flex-col flex-auto gap-[32px] row-start-2 items-center sm:items-start">
+            <div className="list-inside list-decimal text-sm/6 text-center sm:text-left">
+              <div className="mb-2 tracking-[-.01em]">
+                Invalid sign-in link. Please try again.
+              </div>
+            </div>
+          </main>
+        </div>
+      );
+    }
+
+    let email = window.localStorage.getItem('emailForSignIn');
+    if (!email) {
+      email = window.prompt('Please provide your email for confirmation');
+    }
+
+    if (!email) {
+      return (
+        <div className="flex flex-col items-center justify-items-center h-full gap-64">
+          <main className="flex flex-col flex-auto gap-[32px] row-start-2 items-center sm:items-start">
+            <div className="list-inside list-decimal text-sm/6 text-center sm:text-left">
+              <div className="mb-2 tracking-[-.01em]">
+                Email is required to sign in.
+              </div>
+            </div>
+          </main>
+        </div>
+      );
+    }
+
+    signInWithEmailLink(auth, email, currentUrl)
+      .then((userCredential) => {
+        window.localStorage.removeItem('emailForSignIn');
+        logEvent(auth, 'signed_in', {
+          uid: userCredential.user.uid,
+        });
+        redirect("/");
+      }).catch((error) => {
+        console.error("Error signing in with email link:", error);
+        toast.error("Error signing in with email link: " + error.message, {
+          duration: Infinity,
+        });
+      });
+
+
+    return (
+      <div className="flex flex-col items-center justify-items-center h-full gap-64">
+        <main className="flex flex-col flex-auto gap-[32px] row-start-2 items-center sm:items-start">
+          <div className="list-inside list-decimal text-sm/6 text-center sm:text-left">
+            <div className="mb-2 tracking-[-.01em]">
+              Signing in with email link...
+            </div>
+          </div>
+        </main>
+      </div>
+    )
   }
 
   return (
-    <div className="flex flex-col items-center justify-items-center h-full gap-64">
-      <main className="flex flex-col flex-auto gap-[32px] row-start-2 items-center sm:items-start">
-        <div className="list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <div className="mb-2 tracking-[-.01em]">
-            Hello, 
-            {user ? ` ${user.displayName || user.email}` : " Guest"}! Welcome to {appName}.
-          </div>
-        </div>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          {user && (
-            <Button
-              type="button"
-              onClick={handleSignOut}
-              >
-              Sign out
-            </Button>
-          )}
-          {!user && (<>
-            <a
-              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-              href="/login"
-            >
-              Login
-            </a>
-            <a
-              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto"
-              href="/signup"
-            >
-              Sign up
-            </a>
-          </>)}
-        </div>
-      </main>
-      <LinkToAuthfire />
-    </div>
-  );
+    <Suspense fallback={Loading}>
+      <SignIn />
+    </Suspense>
+  )
 }
